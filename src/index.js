4b3e1f306adec39f67b2ae1c905b069216192e41
JayMartMedia <Jay@JayMartMedia.com>
2021-02-02 11:45:13 -0500

    Created initial index.js

---
+++ src/index.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const fetch = require("node-fetch");
+const redis = require("redis");
+
+const PORT = process.env.PORT || 5000;
+const REDIS_PORT = process.env.PORT || 6379;
+
+// must have redis server running
+const client = redis.createClient(REDIS_PORT);
+
+const app = express();
+
+app.listen(5000, () => {
+    console.log(`App listening on port ${PORT}`);
+});
